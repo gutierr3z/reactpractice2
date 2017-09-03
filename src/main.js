2d170228cd4f31682fe4f739a02ba75666798de5
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-03 15:58:26 -0400

    following tutorial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Node Modules
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -10,13 +10,13 @@ import SearchBar from './Components/search_bar'
 const API_KEY = 'AIzaSyBzGahfgQoWXYa9uenNg2ZBVRT6PPH46b0';
 // package : npm install --save youtube-api-search
 // ---------------
-YTSearch({
-    key: API_KEY,
-    term: 'surfboards'
-},
-function( data ) {
-    console.log( data );
-});
+// YTSearch({
+//     key: API_KEY,
+//     term: 'surfboards'
+// },
+// function( data ) {
+//     console.log( data );
+// });
 
 // downwards data flow
  
@@ -27,13 +27,41 @@ function( data ) {
 // });
 
 // Create a new component and produce some html
-const APP = () => {
+// const APP = () => {
 
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+//     return (
+//         <div>
+//             <SearchBar />
+//         </div>
+//     );
+// };
+
+class APP extends Component {
+
+    constructor( props ) {
+        super( props );
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        },
+        ( videos ) => {
+            this.setSate({
+                videos //videos: videos, when key value are the same
+            });
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    };
 };
 
 // take this component's html and put it on the page, in the DOM.
