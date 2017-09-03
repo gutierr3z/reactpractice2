5c6bad91a0ac7a6b3a02122534eb26ea5ff64e1a
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-03 16:46:09 -0400

    following tutorial

--- src/Components/video_list.js
+++ src/Components/video_list.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = ( props ) => {
     // const videos = props;
+    const vidItems = props.videos.map( ( item ) => {
+        return <VideoListItem key = { item.etag } video = { item } />;
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            { props.videos.length }
+            {/* { props.videos.length } */}
+            { vidItems }
         </ul>
     );
 };
