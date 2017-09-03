d5fc6729e274be88dd066818ef790dfdce1cefcb
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-03 13:38:06 -0400

    following tutorial

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -5,8 +5,15 @@ import React, { Component } from 'react';
 // class SearchBar extends React.Component {
 class SearchBar extends Component {
     render() { // always have a render method in classes
-        return <input />; // always return JSX
-    }
+        // return <input onChange = { this.onInputChange } />; // always return JSX
+        // return <input onChange = { ( e ) => console.log( e.target.value ) } />;
+        return <input onChange = { e => console.log( e.target.value ) } />;
+    };
+
+    onInputChange( e ) {
+
+        console.log( e.target.value );
+    };
 };
 
 // functional component
@@ -14,5 +21,11 @@ class SearchBar extends Component {
 
 //     return <input />; // React.createElement
 // };
+
+// handling events in React, 2 step:
+/*
+1 - declare even handler, func
+2 - pass event handler to element we want to monitor for events
+*/
 
 export default SearchBar;
