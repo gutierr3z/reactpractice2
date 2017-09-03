d547198fb27476395cb21d899e4b952384f014af
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-03 14:16:37 -0400

    following tutorial

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -4,10 +4,28 @@ import React, { Component } from 'react';
 // class component
 // class SearchBar extends React.Component {
 class SearchBar extends Component {
+
+    // initializing component's state in a class base component
+    constructor( props ) { // called automatically at instantiation
+        super( props );
+        this.state = {
+            term: ''
+        };
+    };
+
     render() { // always have a render method in classes
         // return <input onChange = { this.onInputChange } />; // always return JSX
         // return <input onChange = { ( e ) => console.log( e.target.value ) } />;
-        return <input onChange = { e => console.log( e.target.value ) } />;
+        // return <input onChange = { e => console.log( e.target.value ) } />;
+        return (
+            <div>
+                {/* <input onChange = { e => this.setState( { term: e.target.value } ) } /> */}
+                <input onChange = { e => this.setState( { term: e.target.value } ) } />
+                <input value = { this.state.term } /> { /* controlled component, value set by state */ }
+                <br />
+                Value of input: { this.state.term }
+            </div>
+        );
     };
 
     onInputChange( e ) {
